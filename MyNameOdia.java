f100ad1e7049781f5e01846aee21351028fcbdac
Raghubeer Prasad Singh <53446322+xraghuveer@users.noreply.github.com>
2026-02-22 22:42:10 +0530

    my name odia

---
+++ MyNameOdia.java
@@ -0,0 +1,15 @@
+package MethodDemo;
+
+public class MyNameOdia {
+    public static void main(String[] args) {
+
+        char c1 = '\u0B30';
+        char c2 = '\u0B18';
+        char c3 = '\u0B41';
+        char c4 = '\u0B2C';
+        char c5 = '\u0B40';
+
+        System.out.println("" + c1 + c2 + c3 + c4 + c5 + c1);
+    }
+}
+
