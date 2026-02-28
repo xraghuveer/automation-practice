4913c234cd38c76054b621a5c75ab16083efe2ab
Raghubeer Prasad Singh <53446322+xraghuveer@users.noreply.github.com>
2026-02-28 16:56:36 +0530

    Define multiple classes for various objects

---
+++ ClassesForObjects.java
@@ -0,0 +1,74 @@
+public class ClassesForObjects {
+    public static void main(String[] args) {
+    }
+}
+class Human{
+    String name;
+    int age;
+    String gender;
+}
+class MobilePhone {
+    String brand;
+    String model;
+    int batteryPercentage;
+}
+class Desktop {
+    String processor;
+    int ram;
+    int storage;
+}
+}
+
+class College {
+    String name;
+    String location;
+    int numOfStudents;
+}
+
+class TestSuite {
+    String suiteName;
+    int totalTestCases;
+    String environment;
+}
+
+class TestCase {
+    String testCaseId;
+    String description;
+    String status;
+}
+
+class TestStep {
+    int stepNumber;
+    String action;
+    String expectedResult;
+}
+
+class Car {
+    String brand;
+    String color;
+    int speed;
+}
+
+class Book {
+    String title;
+    String author;
+    double price;
+}
+
+class Employee {
+    String name;
+    int employeeId;
+    double salary;
+}
+
+class Laptop {
+    String brand;
+    int ram;
+    String processor;
+}
+
+class BankAccount {
+    String HolderName;
+    long accountNumber;
+    double balance;
+}
