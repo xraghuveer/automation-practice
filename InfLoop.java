bcd0e07819ede237ab62636131bf53140d9ddf6e
Raghubeer Prasad Singh <53446322+xraghuveer@users.noreply.github.com>
2026-03-22 00:26:07 +0530

    Create InfLoop.java

---
+++ InfLoop.java
@@ -0,0 +1,15 @@
+public class InfLoop {
+    public static void main(String[] args) {
+        do {
+            System.out.println("Hello");
+        } while (true);
+
+      while (true) {
+            System.out.println("Hello");
+        }
+
+        for (;;) {
+            System.out.println("Hello");
+        }
+    }
+}
